883ae829758a11aed75d4e7ab26eda0422149a8e
chika <chikanobu.kanada@gmail.com>
2020-10-05 22:40:13 -0700

    Completed our build process configuration.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,6 +6,7 @@ import StickyHeader from './modules/StickyHeader'
 
 
 
+
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
